46aa47397557dbd28aa19118f356d14c9f2a089d
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-08-10 17:12:36 +0900

    サーバーを起動できるようになった

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import { getLogger } from 'log4js';
 import * as config from 'config';
-
+import * as Koa from 'koa';
+import * as KoaBodyParser from 'koa-bodyparser';
+import * as KoaRouter from 'koa-router';
+const serverConfig: any = config.get('server');
 const logger = getLogger('Server');
+logger.level = 'info';
+
+const application = new Koa();
+application.use(KoaBodyParser());
+
+application.listen(serverConfig.listen);
+logger.info(`Listen on ${serverConfig.listen}`);

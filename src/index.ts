d24d6a8f277c69609b75d465150544a22dc73db0
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-09-05 17:52:04 +0900

    スタイルを調整した

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,35 @@ import * as KoaRouter from 'koa-router';
 import * as KoaStatic from 'koa-static';
 import defineRoutes from '@main/routes';
 import { createLogger } from '@lib/utility';
+const logger = createLogger('Server');
 
 const currentDirectory = process.cwd();
 const serverConfig: any = config.get('server');
-const logger = createLogger('Server');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const application = new Koa();
 const router = new KoaRouter();
 defineRoutes(router);
 
-application.use(
-  KoaBodyParser({
-    extendTypes: {
-      json: ['application/activity+json', 'application/ld+json'],
-    },
-  }),
-);
-application.use(router.routes());
-application.use(router.allowedMethods());
-application.use(KoaStatic(path.resolve(currentDirectory, 'dist/public')));
-application.listen(serverConfig.listen);
-logger.info(`Listen on ${serverConfig.listen}`);
+run();
+
+async function run() {
+  if (isDevelopment) {
+    const KoaWebpack = await import('koa-webpack');
+    const koaWebpack = await KoaWebpack();
+    application.use(koaWebpack);
+    application.use(KoaStatic(path.resolve(currentDirectory, 'dist/public')));
+  }
+
+  application.use(
+    KoaBodyParser({
+      extendTypes: {
+        json: ['application/activity+json', 'application/ld+json'],
+      },
+    }),
+  );
+  application.use(router.routes());
+  application.use(router.allowedMethods());
+  application.listen(serverConfig.listen);
+  logger.info(`Listen on ${serverConfig.listen}`);
+}

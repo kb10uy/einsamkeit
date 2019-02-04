7d5fdca73306f8c2de275ae1cc4f3572143a93cf
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-02-04 16:58:17 +0900

    ユーザー登録を実装

--- server/blahctl.ts
+++ server/blahctl.ts
@@ -1,24 +1,28 @@
 import * as fs from 'fs';
 import { promisify } from 'util';
 import * as cac from 'cac';
-import { getKnex } from './util';
+import { getLogger, getKnex } from './util';
+
+async function addUser(username: string, publicKeyFilename: string, privateKeyFilename: string): Promise<void> {
+  const logger = getLogger();
+  const knex = getKnex();
+  try {
+    const pubkey = await promisify(fs.readFile)(publicKeyFilename);
+    const prvkey = await promisify(fs.readFile)(privateKeyFilename);
+    const now = new Date();
+    await knex('users').insert({
+      name: username,
+      key_public: pubkey.toString(),
+      key_private: prvkey.toString(),
+      created_at: now,
+      updated_at: now,
+    });
+    logger.info(`User ${username} registered`);
+  } catch (e) {
+    console.log(e.message);
+  }
+}
 
 const blahctl = cac('blahctl');
-
-blahctl
-  .command('add-user <username> <public key> <private key>', 'adds a new local account')
-  .action(async (username: string, publicKeyFilename: string, privateKeyFilename: string) => {
-    console.log(`username: ${username}`);
-    console.log(`pub: ${publicKeyFilename}`);
-    console.log(`prv: ${privateKeyFilename}`);
-
-    try {
-      const pubkey = await promisify(fs.readFile)(publicKeyFilename);
-      const prvkey = await promisify(fs.readFile)(privateKeyFilename);
-      console.log(pubkey.toString());
-    } catch (e) {
-      console.log(e.message);
-    }
-  });
-
+blahctl.command('add-user <username> <public key> <private key>', 'adds a new local account').action(addUser);
 blahctl.parse();

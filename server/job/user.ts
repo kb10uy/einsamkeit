d72c2965f8aa6da0b745b3c9e40aeac1226b2792
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-02-09 21:18:26 +0900

    フォロー情報更新

--- server/job/user.ts
+++ server/job/user.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ReceiveFollowJob, SendAcceptJob } from './types';
 import { resolveLocalUser, fetchRemoteUser } from '../action/user';
-import { getQueue, getLogger, resolveLocalUrl, getRedis } from '../util';
+import { getQueue, getLogger, resolveLocalUrl, getRedis, getKnex } from '../util';
 import { generateHttpSignature } from '../action/auth';
 import { URL } from 'url';
 import { makeASRoot } from '../ap/activitystreams';
@@ -9,6 +9,7 @@ import { makeASRoot } from '../ap/activitystreams';
 const logger = getLogger();
 const queue = getQueue();
 const redis = getRedis();
+const knex = getKnex();
 
 /**
  * Follow Activity の処理
@@ -21,24 +22,42 @@ export async function receiveFollow(data: ReceiveFollowJob): Promise<void> {
   const target = await resolveLocalUser(data.target);
   if (!target) throw new Error(`User ${data.target} not found`);
 
-  // 自動 Accept なので送るだけ
   const actor = await fetchRemoteUser(data.actor);
   const inbox = actor.server.sharedInbox || actor.inbox;
-  await queue.add({
-    id: '',
-    type: 'sendAccept',
-    targetInbox: inbox,
-    privateKey: {
-      key: target.privateKey,
-      id: resolveLocalUrl(`/users/${target.name}#publickey`),
-    },
-    actor: data.target,
-    object: {
-      type: 'Follow',
-      actor: data.actor,
-      object: data.target,
-    },
-  });
+  const [follower] = await knex('followers')
+    .select('id')
+    .where('local_user_id', target.id)
+    .where('remote_user_id', actor.id);
+  if (follower) {
+    logger.info(`Remote user #${actor.id} is already follwing local user #${target.id}`);
+  } else {
+    const now = new Date();
+    // Redis と DB のフォロワー情報を更新
+    await redis.hincrby(`userstats:${target.id}`, 'followers', 1);
+    await knex('followers').insert({
+      local_user_id: target.id,
+      remote_user_id: actor.id,
+      created_at: now,
+      updated_at: now,
+    });
+
+    // 自動 Accept なので送るだけ
+    await queue.add({
+      id: '',
+      type: 'sendAccept',
+      targetInbox: inbox,
+      privateKey: {
+        key: target.privateKey,
+        id: resolveLocalUrl(`/users/${target.name}#publickey`),
+      },
+      actor: data.target,
+      object: {
+        type: 'Follow',
+        actor: data.actor,
+        object: data.target,
+      },
+    });
+  }
 }
 
 export async function sendAccept(data: SendAcceptJob): Promise<void> {

92a0de647df598a87588b0817cb2cbbe50e7efe5
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-02-09 21:58:55 +0900

    OrderedCollectionを追加

--- server/ap/activitystreams.ts
+++ server/ap/activitystreams.ts
@@ -1,5 +1,15 @@
+/**
+ * ActivityStreams の基底オブジェクト
+ */
 export function makeASRoot(): any {
   return {
     '@context': ['https://www.w3.org/ns/activitystreams', 'https://w3id.org/security/v1'],
+  };
+}
+
+export function makeASOrderedCollection(items: any[]): any {
+  return {
+    ...makeASRoot(),
+    type: 'OrderedCollection',
   };
 }

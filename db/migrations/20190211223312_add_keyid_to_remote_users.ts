553ff59dc3bebdf99d18aa4766deeb6502d0fc21
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-02-11 22:35:16 +0900

    keyIdを保存できるようにする

---
+++ db/migrations/20190211223312_add_keyid_to_remote_users.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('remote_users', (table) => {
+    table.text('key_id').nullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('remote_users', (table) => {
+    table.dropColumn('key_id');
+  });
+}

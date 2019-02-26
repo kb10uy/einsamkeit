d6f1fce49e7a9e16ed4233297b1e10cdf09850ca
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-02-26 15:49:02 +0900

    メッセージの治安を悪化させた

--- server/web/auth.ts
+++ server/web/auth.ts
@@ -1,19 +1,28 @@
 import * as bcrypt from 'bcrypt';
 import { stores } from 'koa-session';
-import { EinsamkeitContext, EinsamkeitMiddleware } from '../types';
-import { setSuccess, renderPug, getRedis, getKnex } from '../util';
+import { EinsamkeitContext } from '../types';
+import { setSuccess, renderPug, getRedis, getKnex, addErrorFlash, addInformationFlash } from '../util';
 
 const knex = getKnex();
 const redis = getRedis();
 
-export async function checkAuthenticated(context: EinsamkeitContext, next: () => Promise<any>): Promise<void> {}
+export async function checkAuthenticated(context: EinsamkeitContext, next: () => Promise<any>): Promise<void> {
+  if (!context || !context.session) throw new Error('Precondition failed');
+  if (context.session.user === undefined) {
+    addErrorFlash(context, 'ログインしろボケナス');
+    context.redirect('/auth/login');
+  }
+  await next();
+}
 
 export async function showLogin(context: EinsamkeitContext): Promise<void> {
+  if (!context.session) throw new Error('Precondition failed');
   setSuccess(
     context,
     200,
     renderPug('login.pug', {
       csrfToken: context.csrf,
+      flash: context.session.flash,
     }),
   );
 }
@@ -23,6 +32,7 @@ export async function tryLogin(context: EinsamkeitContext): Promise<void> {
 
   const { username, password } = context.request.body;
   if (!username || !password) {
+    addErrorFlash(context, '節穴アイか? input 要素ぐらいちゃんと見ろボケ');
     context.redirect('/auth/login');
   }
 
@@ -30,11 +40,13 @@ export async function tryLogin(context: EinsamkeitContext): Promise<void> {
     .select()
     .where('name', username);
   if (!user) {
+    addErrorFlash(context, 'さてはアンチだなオメー');
     context.redirect('/auth/login');
   }
 
   const matchesPassword = await bcrypt.compare(password, user.password_hash);
   if (!matchesPassword) {
+    addErrorFlash(context, 'お?パスワード忘れたか?');
     context.redirect('/auth/login');
   }
 
@@ -45,15 +57,16 @@ export async function logout(context: EinsamkeitContext): Promise<void> {
   if (!context.session) throw new Error('Precondition failed');
 
   context.session.user = undefined;
+  addInformationFlash(context, 'またね〜');
   context.redirect('/');
 }
 
 export const sessionStore: stores = {
-  async get(key, maxAge, { rolling }) {
+  async get(key) {
     const data = await redis.get(key);
     return data ? JSON.parse(data) : {};
   },
-  async set(key, sess, maxAge, { rolling, changed }) {
+  async set(key, sess, maxAge) {
     const data = JSON.stringify(sess);
     await redis.set(key, data);
     if (typeof maxAge === 'number') await redis.expire(key, maxAge);

a3db80dc6f31f62790fc482854d232f064b8219e
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-03-02 13:06:42 +0900

    topbarの色を調整

--- client/admin/components/top-bar.tsx
+++ client/admin/components/top-bar.tsx
@@ -10,6 +10,8 @@ const Container = styled.div`
 `;
 
 const Logo = styled.div`
+  font-family: 'Playball';
+  color: #fff;
   grid-column: 1;
   font-size: 24pt;
   margin: 0 10px;
@@ -25,24 +27,30 @@ const Logout = styled.a`
   height: 48px;
   line-height: 48px;
   transition: 0.2s background-color;
+  color: #fff;
+  padding: 0 10px;
+  text-decoration: none;
   &:hover {
     background-color: rgba(0, 0, 0, 0.2);
   }
 `;
 
-function callLogout() {}
-
 export default () => {
   const csrfMeta: HTMLMetaElement | null = document.querySelector('meta[name=csrf-token]');
   const csrfToken = csrfMeta ? csrfMeta.getAttribute('content') || '' : '';
+  const logout = () => {
+    const logout = document.querySelector('form#logout_form');
+    if (logout) (logout as HTMLFormElement).submit();
+  };
+
   return (
     <Container>
       <Logo>Einsamkeit</Logo>
       <RightMenu>
         <Logout href="#">フェイク</Logout>
-        <form action="/auth/logout" method="post">
+        <Logout onClick={logout}>ログアウト</Logout>
+        <form action="/auth/logout" method="post" id="logout_form">
           <input type="hidden" name="_csrf" value={csrfToken} />
-          <Logout>ログアウト</Logout>
         </form>
       </RightMenu>
     </Container>

355af499e64e9e7e9dab1cbf103d661d5375191e
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-03-02 14:02:37 +0900

    治癒

--- client/admin/components/top-bar.tsx
+++ client/admin/components/top-bar.tsx
@@ -3,33 +3,34 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: max-content 1fr max-content;
   grid-template-rows: 48px;
+  grid-template-columns: max-content 1fr max-content;
   background-color: #ffa552;
   filter: drop-shadow(0 0 8px rgba(0, 0, 0, 0.4));
 `;
 
 const Logo = styled.div`
-  font-family: 'Playball';
-  color: #fff;
   grid-column: 1;
-  font-size: 24pt;
   margin: 0 10px;
+  font-family: 'Playball', serif;
+  font-size: 24pt;
+  color: #fff;
 `;
 
 const RightMenu = styled.div`
-  grid-column: 3;
   display: flex;
   flex-direction: row;
+  grid-column: 3;
 `;
 
 const Logout = styled.a`
   height: 48px;
-  line-height: 48px;
-  transition: 0.2s background-color;
-  color: #fff;
   padding: 0 10px;
+  line-height: 48px;
+  color: #fff;
   text-decoration: none;
+  transition: 0.2s background-color;
+
   &:hover {
     background-color: rgba(0, 0, 0, 0.2);
   }
@@ -38,7 +39,7 @@ const Logout = styled.a`
 export default () => {
   const csrfMeta: HTMLMetaElement | null = document.querySelector('meta[name=csrf-token]');
   const csrfToken = csrfMeta ? csrfMeta.getAttribute('content') || '' : '';
-  const logout = () => {
+  const logout = (): void => {
     const logout = document.querySelector('form#logout_form');
     if (logout) (logout as HTMLFormElement).submit();
   };
--- server/web/user.ts
+++ server/web/user.ts
@@ -1,3 +1,3 @@
-import { EinsamkeitContext, EinsamkeitMiddleware } from '../types';
+import { EinsamkeitContext } from '../types';
 
-export async function user(context: EinsamkeitContext): Promise<void> {}
+export async function user(context: EinsamkeitContext): Promise<void> { }
